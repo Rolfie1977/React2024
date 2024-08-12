c34e3e2af4bfcfc5035dc2c52c08c49600e74930
Rolf M <rolfie@outlook.dk>
2024-08-12 12:04:51 +0200

    Reactny

---
+++ first-react/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<footer>
+			<p>&copy; 2024 Rolfs Hjemmeside. Alle rettigheder forbeholdes.</p>
+		</footer>
+	);
+}
---
+++ first-react/src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Main() {
+	return (
+		<main>
+			<p>Rolfs main.</p>
+		</main>
+	);
+}
