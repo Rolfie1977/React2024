cf107f92f2b0668b9164d65e76828ff9141b2b05
Rolf M <rolfie@outlook.dk>
2024-08-13 10:41:01 +0200

    PropsCool

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -4,15 +4,18 @@ import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 import { Footer } from "./components/Footer";
 import { Greeting } from "./components/Greeting";
+import { CustomHeader } from "./components/CustomHeader"; // Import af CustomHeader
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Main>
+				{/* Brug af CustomHeader med prop */}
+				<CustomHeader headerText="Props er cool" />
 				{/* Tilføj en enkelt Greeting komponent med ét navn */}
 				<Greeting name="Rolf" />
-							</Main>
+			</Main>
 			<Footer />
 		</>
 	);
---
+++ first-react/src/components/CustomHeader.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function CustomHeader(props) {
+    return (
+        <h1>{props.headerText}</h1>
+    );
+}
